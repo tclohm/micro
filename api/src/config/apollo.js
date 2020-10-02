7b3143d27e06fe9cc46e982fce904ec051ba6927
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-10-02 10:18:45 -0700

    pass context from gateway to account services

--- api/src/config/apollo.js
+++ api/src/config/apollo.js
@@ -1,15 +1,30 @@
-import  { ApolloGateway } from "@apollo/gateway";
+import  { ApolloGateway, RemoteGraphQLDataSource } from "@apollo/gateway";
 import { ApolloServer } from "apollo-server-express";
 
 const gateway = new ApolloGateway({
 	serviceList: [
 		{ name: "accounts", url: process.env.ACCOUNTS_SERVICE_URL }
-	]
+	],
+	buildService({ url }) {
+		return new RemoteGraphQLDataSource({
+			url,
+			willSendRequest({ request, context }) {
+				request.http.headers.set(
+					"user",
+					context.user ? JSON.stringify(context.user) : null
+				);
+			}
+		});
+	}
 })
 
 const server = new ApolloServer({
 	gateway,
-	subscriptions: false
-})
+	subscriptions: false,
+	context: ({ req }) => {
+		const user = req.user || null
+		return { user }
+	}
+});
 
 export default server;
--- api/src/config/app.js
+++ api/src/config/app.js
@@ -1,5 +1,28 @@
 import express from "express";
+import jwt from "express-jwt";
+import jwksClient from "jwks-rsa";
 
 const app = express()
+
+const jwtCheck = jwt({
+	secrets: jwtsClient.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `${process.env.AUTH0_ISSUER}.well-known/jwks.json`
+	}),
+	audience: process.env.AUTH0_AUDIENCE,
+	issuer: process.env.AUTH0_ISSUER,
+	algorithms: ["RS256"],
+	credentialsRequired: false
+});
+
+// MARK: -- manage auth access
+app.use(jwtCheck, (err, req, res, next) => {
+	if (err.code == "invalid_token") {
+		return next()
+	}
+	return next(err)
+});
 
 export default app;
