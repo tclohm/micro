1df396f02740517a08d22650af0c4f4953937d20
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-10-07 11:29:05 -0700

    added a accounts datasource and dependency inject into resolver. placed descriptions in typedefs

--- api/src/services/accounts/typeDefs.js
+++ api/src/services/accounts/typeDefs.js
@@ -1,48 +1,86 @@
 import { gql } from "apollo-server";
 
 const typeDefs = gql`
-
+	"""
+	An ISO 8601-encoded UTC date string.
+	"""
 	scalar DateTime
 	"""
 	An account is an Auth0 user that provides authentication details.
 	"""
 	type Account @key(fields: "id") {
+		"unqiue Auth0 ID associated with the account"
 		id: ID!
+		"The date and time the account was created"
 		createdAt: DateTime!
+		"Email associated with the account (unique)"
 		email: String!
+		"Account blocked"
 		isBlocked: Boolean
+		"Account has moderator role"
 		isModerator: Boolean
 	}
 
 	extend type Query {
+		"Retrieves a single account by Auth0 ID."
 		account(id: ID!): Account!
+		"Retrieves a list of accounts."
 		accounts: [Account]
+		"Retrieves the current logges in account from Auth0."
 		viewer: Account
 	}
 
+	"""
+	Provides data to create new account
+	"""
 	input CreateAccountInput {
+		"New account's email (unique)"
 		email: String!
+		"New account's password"
 		password: String!
 	}
 
+	"""
+	Provides the unique ID of an existing account
+	"""
 	input AccountWhereUniqueInput {
+		"Unique Auth0 ID associated with the account"
 		id: ID!
 	}
 
+	"""
+	Provides data to update an exisiting account.
+
+	A current password and new password are required to update a password.
+
+	Password and email fields cannot be update simultaneously.
+	"""
 	input UpdateAccountInput {
+		"The updated account email."
 		email: String
+		"The updated account password."
 		newPassword: String
+		"The exisiting account password."
 		password: String
 	}
 
 	extend type Mutation {
+		"Toggles permissions of moderator role."
 		changeAccountModeratorRole(where: AccountWhereUniqueInput!): Account!
+
+		"Creates a new account."
 		createAccount(data: CreateAccountInput): Account!
+
+		"Deletes an account."
 		deleteAccount(where: AccountWhereUniqueInput): Boolean!
+
+		"Updates an account's details."
 		updateAccount(
 			data: UpdateAccountInput!
 			where: AccountWhereUniqueInput!
 		): Account!
+
+		"Blocks or unblocks an account from authenticating."
 		changeAccountBlockedStatus(where: AccountWhereUniqueInput!): Account!
 	}
 `
