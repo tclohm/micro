a44447ec0aa53463c963fe993637325ce798478a
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-10-22 16:12:40 -0700

    sends AccentButton with props and style into pages/Index

--- web/src/pages/Index/index.js
+++ web/src/pages/Index/index.js
@@ -1,11 +1,23 @@
+import { Box } from "grommet";
+import { ChatOption } from "grommet-icons";
 import React from "react";
 
+import AccentButton from "../../components/AccentButton";
 import MainLayout from "../../layouts/MainLayout";
 
 const Index = () => {
 	return (
-		<MainLayout>
-			<p>This is the index page.</p>
+		<MainLayout centered>
+			<Box align="center" margin={{ top: "small" }} width="100%">
+				<ChatOption color="brand" size="300px" />
+				<div>
+					<AccentButton
+						label="Login / Sign-up"
+						margin={{ top: "medium" }}
+						onClick={() => console.log("Clicked!")}
+					/>
+				</div>
+			</Box>
 		</MainLayout>
 	);
 };
