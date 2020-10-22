61d1ad008c0494de81d15ff1352cfff3fcfc4536
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-10-22 12:43:01 -0700

    deactivated apollo's default cors setting

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,7 @@ import server from "./config/apollo";
 
 const port = process.env.PORT
 
-server.applyMiddleware({ app })
+server.applyMiddleware({ app, cors: false })
 
 app.listen({ port }, () => {
 	console.log(` ✔︎ server is up ${port}${server.graphqlPath}`)
