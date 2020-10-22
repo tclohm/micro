2db59ebc4eb6640bf7c393d522db5d835ab5a2e1
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-10-22 12:41:28 -0700

    added cors to middleware

--- api/src/config/app.js
+++ api/src/config/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import jwt from "express-jwt";
 import jwksClient from "jwks-rsa";
+import cors from "cors";
 
 const app = express()
 
@@ -24,5 +25,9 @@ app.use(jwtCheck, (err, req, res, next) => {
 	}
 	return next(err)
 });
+
+if (process.env.NODE_ENV === "development") {
+	app.use(cors({ origin: "http://localhost:3000" }));
+}
 
 export default app;
