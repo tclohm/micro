7eb2819b252b43c35162b0f33ddb9d6bc2145654
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-10-22 14:40:54 -0700

    added navbar and footer

---
+++ web/src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import { Box, Text } from "grommet";
+import React from "react";
+
+const Footer = () => (
+	<footer>
+		<Box
+			direction="row"
+			justify="center"
+			margin={{ top: "medium", bottom: "small" }}
+		>
+			<Text as="p">
+				MicroFails is Discarded Floppy Disk{" "}
+				<span role="img" aria-label="Floppy Disk and Trash bin emoji">
+					💾 => 🗑
+				</span>
+			</Text>
+		</Box>
+	</footer>
+);
+
+export default Footer;
