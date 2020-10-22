9704a5c8e42c31a87561212df9691d1d1ab2899d
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-10-21 20:24:45 -0700

    added grommet to the web package

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { ApolloProvider } from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import client from "./graphql/apollo";
 
 import "./index.css";
 
 const App = () => (
-	<div>
-		<p>Hello, world!</p>
-	</div>
+	<ApolloProvider client={client}>
+		<div>
+			<p>Hello, world!</p>
+		</div>
+	</ApolloProvider>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
