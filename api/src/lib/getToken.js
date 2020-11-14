cb75f99e2fc05926bf3ebec629548c2687d5ed6f
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-11-14 14:36:15 -0800

    added isomorphic-fetch and updated getToken to grab token from database

--- api/src/lib/getToken.js
+++ api/src/lib/getToken.js
@@ -1,34 +1,35 @@
-import { gql } from "apollo-server";
-import request from "request";
-import util from "util";
+import fetch from "isomorphic-fetch";
 
-const requestPromise = util.promisify(request);
+export default async function getToken(email, password) {
 
-export default async function (email, password) {
-	const query = { 
-				"query": 
-				`mutation { authentication(email: ${email}, password: ${password})`
-			}
-	const options = {
-		method: "POST",
-		url: `http://localhost:${process.env.PORT}/graphql`,
-		headers: { "content-type": "application/graphql" },
-		body: query
-	};
+	const query = `mutation auth($email: String!, $password: String!) {
+		    		authenticate(
+		    			data: { email: $email, password: $password }
+		    		) { 
+		    			refreshToken 
+		    		} 
+		    	}
+		    `
 
-	const response = await requestPromise(options).catch(error => {
-		throw new Error(error);
-	});
+	const variables = { email: email, password: password };
 
-	console.log(response.body);
-
+	const result = await fetch('http://localhost:4000/graphql', 
+	{
+		method: 'post',
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({query, variables})
+	})
+		.then(response => {
+			return response.json() 
+		})
+		.then(data => {
+			return data
+		})
+		.catch(e => {
+			console.log(e)
+		})
+	
+	return result.data.authenticate.refreshToken
 }
-
-
-// const token = gql`
-// 	mutation Authenticate($email: email, $password: password) {
-// 		authenticate(email: $email, password: $password) {
-// 			refreshToken
-// 		}
-// 	}
-// `;
--- api/src/scripts/authenticateUser.js
+++ api/src/scripts/authenticateUser.js
@@ -2,6 +2,6 @@ import getToken from "../lib/getToken";
 
 (async () => {
 	const [email, password] = process.argv.slice(2)
-	const accessToken = getToken(email, password)
+	const accessToken = await getToken(email, password)
 	console.log(accessToken);
 })();
